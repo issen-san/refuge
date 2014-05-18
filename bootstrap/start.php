f7ca6fc631db2e72ad2ddb83b725232dbcded423
issen-san <admin@cloxs.jp>
2014-05-18 21:24:10 +0900

    refuge/refuse/のファイルをrefuge/に置く

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -24,11 +24,11 @@ $app = new Illuminate\Foundation\Application;
 |
 */
 
-$env = $app->detectEnvironment(array(
+$env = $app->detectEnvironment(function() {
 
-	'local' => array('your-machine-name'),
+    return isset($_SERVER['LARAVEL_ENV']) ? $_SERVER['LARAVEL_ENV'] : 'local';
 
-));
+});
 
 /*
 |--------------------------------------------------------------------------
